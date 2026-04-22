03161b35b1070cd422155c8a4b7e56a807f6d14c
MalicknND <ndiayemalick062@gmail.com>
2026-04-22 16:22:02 +0200

    Ajout de la gestion des données de carte et des événements de redimensionnement dans le composant App.

--- Projets/react-typescript/src/App.tsx
+++ Projets/react-typescript/src/App.tsx
@@ -1,10 +1,36 @@
 import "./App.css";
 import Card from "./components/Card";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [cardsdata, setCardsdata] = useState([
+    { title: "la carte 1", content: "le contenu 1", id: "1" },
+    { title: "la carte 2", content: "le contenu 2", id: "2" },
+    { title: "la carte 3", content: "le contenu 3", id: "3" },
+    { title: "la carte 4", content: "le contenu 4", id: "4" },
+  ]);
+
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  console.log(btnRef);
+
+  useEffect(() => {
+    const handleResize = (e: Event) => {
+      console.log("Window resized", e);
+    };
+    console.log(btnRef);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="App">
-      <Card title="la carte" content="le contenu" />
+      {cardsdata.map((card) => (
+        <Card key={card.id} title={card.title} content={card.content} />
+      ))}
+      <button ref={btnRef}>Submit</button>
     </div>
   );
 }
