9cb0fe83721bebb27d5c518b28de34f9e9a516c0
MalicknND <ndiayemalick062@gmail.com>
2026-04-22 15:33:08 +0200

    feat: add initial setup for React TypeScript project with Vite
    - Added SVG assets for React and Vite logos
    - Created a Card component with title and content props
    - Added global CSS styles for the application
    - Set up main entry point for React application
    - Configured TypeScript with app and node configurations
    - Created Vite configuration file for project bundling

---
+++ Projets/react-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Card title="la carte" content="le contenu" />
+    </div>
+  );
+}
+
+export default App;
