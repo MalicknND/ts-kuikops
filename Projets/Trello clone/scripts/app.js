14982e9a58135bc30c53df0b3ae223a443168fc2
MalicknND <ndiayemalick062@gmail.com>
2026-04-22 10:32:31 +0200

    Ajout de la fonctionnalité de création de nouveaux conteneurs dans l'application Trello Clone

--- Projets/Trello clone/scripts/app.js
+++ Projets/Trello clone/scripts/app.js
@@ -91,4 +91,54 @@ function handleAddItemDeletion(btn) {
         elToRemove.remove();
     });
 }
+/// Add New Container
+const addContainerBtn = document.querySelector(".add-container-btn");
+const addContainerForm = document.querySelector(".add-new-container form");
+const addContainerFormInput = document.querySelector(".add-new-container input");
+const validationNewContainer = document.querySelector(".add-new-container .validation-msg");
+const addContainerCloseBtn = document.querySelector(".close-add-list");
+const addNewContainer = document.querySelector(".add-new-container");
+const containersList = document.querySelector(".main-content");
+addContainerBtn.addEventListener("click", () => {
+    toggleForm(addContainerBtn, addContainerForm, true);
+});
+addContainerCloseBtn.addEventListener("click", () => {
+    toggleForm(addContainerBtn, addContainerForm, false);
+});
+addContainerForm.addEventListener("submit", createNewContainer);
+function createNewContainer(event) {
+    event.preventDefault();
+    if (addContainerFormInput.value.length === 0) {
+        validationNewContainer.textContent = "Must be at least 1 character long";
+        return;
+    }
+    else {
+        validationNewContainer.textContent = "";
+    }
+    const itemsContainer = document.querySelector(".items-container");
+    const newContainer = itemsContainer.cloneNode(true);
+    const newContainerContent = `
+         
+        <div class="top-container">
+          <h2>${addContainerFormInput.value}</h2>
+          <button class="delete-container-btn">X</button>
+        </div>
+        <ul></ul>
+        <button class="add-item-btn">Add an item</button>
+        <form autocomplete="off">
+          <div class="top-form-container">
+            <label for="item">Add a new item</label>
+            <button type="button" class="close-form-btn">X</button>
+          </div>
+          <input type="text" id="item" />
+          <span class="validation-msg"></span>
+          <button type="submit">Submit</button>
+        </form>
+      
+  `;
+    newContainer.innerHTML = newContainerContent;
+    containersList.insertBefore(newContainer, addNewContainer);
+    addContainerFormInput.value = "";
+    addContainerListerner(newContainer);
+}
 export {};
